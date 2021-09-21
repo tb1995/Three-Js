0d8527b8463de11814932254b8be550adbf694a2
tb1995 <tb1995@gmail.com>
2021-09-21 19:12:21 +0500

    4 new patterns

--- Shader-Karate/exercise/src/shaders/fragment.glsl
+++ Shader-Karate/exercise/src/shaders/fragment.glsl
@@ -3,6 +3,7 @@ precision mediump float;
 
 varying vec2 vUv;
 float strength;
+float strength2;
 
 void main() {
 
@@ -31,9 +32,31 @@ void main() {
     // strength = step(mod(vUv.y * 10.0, 1.0), 0.2);
 
     // same as above, but lines are vertical instead of horizontal
-    strength = step(mod(vUv.x * 10.0, 1.0), 0.2);
+    // strength = step(mod(vUv.x * 10.0, 1.0), 0.2);
+
+    // combining the two, kind of makes it a waffle. If you check later to see that it doesn't match the exact pattern in the exercise, it's because you need to flip the arguments in step
+    // strength = step(mod(vUv.x * 10.0, 1.0), 0.2);
+    // strength = strength + (step(mod(vUv.y * 10.0, 1.0), 0.2));
+
+    // square grid
+    // strength = step(mod(vUv.x * 10.0, 1.0), 0.2);
+    // strength = strength * step(mod(vUv.y * 10.0, 1.0), 0.2);
+
+    // horizontal dashed pattern
+     strength2 = step(mod(vUv.x * 10.0, 1.0), 0.3);
+     strength2 = strength2 * (step(mod(vUv.y * 10.0, 1.0), 0.7));
+
+
+    strength = step(mod(vUv.x * 10.0, 1.0), 0.7);
+    strength = (strength * step(mod(vUv.y * 10.0, 1.0), 0.3));// * strength2;
+    strength = strength2 + strength;
+    // strength = 1.0 - strength;
+
+
+
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
+
 
 
 
